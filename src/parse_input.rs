8a59951fa3e6809b036fa6ba416ca45972665bd8
Trevan Haskell <dev@trevanhaskell.com>
2020-06-13 14:19:05 -0600

    0.3.4 - parse_input documentation

--- src/parse_input.rs
+++ src/parse_input.rs
@@ -5,7 +5,7 @@ use crate::print_debug;
 use crate::write_out::{type_text, Color};
 
 /// Yes, No, or Unsure. Corresponds with a dictionary of responses that indicate one of these three answers.
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Answer {
     /// Yes, y, yeah, sure, etc.
     Yes,
@@ -15,8 +15,8 @@ pub enum Answer {
     Unsure,
 }
 
-/// Cardinal directions, as well as Up, Down, and Return. Corresponds with a dictionary of responses..
-#[derive(Debug)]
+/// Cardinal directions, as well as Up, Down, and Return. Corresponds with a dictionary of responses.
+#[derive(Debug, PartialEq)]
 pub enum Direction {
     /// North, n, forward, etc.
     North,
@@ -186,6 +186,18 @@ const LOADS: &[&str] = &[
     "load game",
 ];
 
+/// Returns true if the given dictionary contains the given input string.
+/// 
+/// ```
+/// # use intfic::parse_input::query;
+/// assert_eq!(query("@AFFIRMATIVES", "sure"), true);
+/// ```
+/// Dictionaries are specified with an @ sign in front of them, and can be referenced this way in the "typed" field of a Choice.
+/// ```text
+///   Isn't that neat?
+/// *- Yeah I guess. -> @AFFIRMATIVES -> neat
+/// *- Not really. -> @NEGATIVES -> not_neat
+/// ```
 pub fn query(dict: &str, name: &str) -> bool {
     match dict {
         "@AFFIRMATIVES" => AFFIRMATIVES.contains(&name),
@@ -205,6 +217,12 @@ pub fn query(dict: &str, name: &str) -> bool {
     }
 }
 
+/// Returns a lowercase string containing only alphanumeric characters and spaces.
+/// 
+/// ```
+/// # use intfic::parse_input::sanitize;
+/// 
+/// assert_eq!(sanitize(String::from("OH mOst DefiniTEly!?!")), String::from("oh most definitely"));
 pub fn sanitize(input: String) -> String {
     input
         .chars()
@@ -214,6 +232,21 @@ pub fn sanitize(input: String) -> String {
         .to_lowercase()
 }
 
+/// Gets input from the user and checks if it matches a keyword (returns None) or else returns Some(String).
+/// 
+/// Keywords are the following:
+/// * exit - asks to save if you haven't recently, then quits the game. See [game_state::GameState::quit()](../game_state/struct.GameState.html#method.quit)
+/// * save - saves the game. See [game_state::GameState::save()](../game_state/struct.GameState.html#method.save)
+/// * load - loads the game. See [game_state::GameState::load()](../game_state/struct.GameState.html#method.load)
+/// 
+/// ```no_run
+/// # use intfic::parse_input::get_input;
+/// # use intfic::game_state::GameState;
+/// let mut game: GameState = GameState::new("Test GameState");
+/// 
+/// assert_eq!(get_input(&mut game), Some(String::from("open door"))); // If the user typed "open door"
+/// assert_eq!(get_input(&mut game), None); // If the user typed "load" or "save"
+/// ```
 pub fn get_input(game: &mut GameState) -> Option<String> {
     let input: String = sanitize(read!("{}\n"));
 
@@ -258,6 +291,19 @@ fn print_parse_result(input: &str, parsed: &str) {
     print_debug(format!("Input: {}, Parsed: {}", input, parsed));
 }
 
+/// Asks a given yes-no question and returns Some(Answer) if the user doesn't type a keyword.
+/// 
+/// If the user types something that does not correspond to any of the Answer dictionaries, 
+/// the question will repeat until a proper response or keyword is given.
+/// 
+/// ```no_run
+/// # use intfic::parse_input::{ask_question, Answer};
+/// # use intfic::game_state::GameState;
+/// let mut game: GameState = GameState::new("Test GameState");
+/// 
+/// assert_eq!(ask_question("Continue?", &mut game), Some(Answer::Yes)); // If the user typed "y"
+/// assert_eq!(ask_question("Continue?", &mut game), None); // If the user typed "load" or "save"
+/// ```
 pub fn ask_question(question: &str, game: &mut GameState) -> Option<Answer> {
     loop {
         type_text(question, Color::Cyan, true);
@@ -293,6 +339,19 @@ fn parse_answer(input: &str) -> Option<Answer> {
     }
 }
 
+/// Asks for a direction and returns Some(Answer) if the user doesn't type a keyword.
+/// 
+/// If the user types something that does not correspond to any of the Direction dictionaries, 
+/// the question will repeat until a proper response or keyword is given.
+/// 
+/// ```no_run
+/// # use intfic::parse_input::{ask_direction, Direction};
+/// # use intfic::game_state::GameState;
+/// let mut game: GameState = GameState::new("Test GameState");
+/// 
+/// assert_eq!(ask_direction("Which Way?", &mut game), Some(Direction::North)); // If the user typed "Northward!"
+/// assert_eq!(ask_direction("Which Way??", &mut game), None); // If the user typed "load" or "save"
+/// ```
 pub fn ask_direction(question: &str, game: &mut GameState) -> Option<Direction> {
     loop {
         type_text(question, Color::Cyan, true);
